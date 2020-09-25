dece4b8a69865e6f214c9dbed1bd874b5885b877
humphrey <humphrey_hu@163.com>
2020-09-25 22:06:53 +0800

    Master react route demo.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import Home from './components/Home/Home';
+import Error404 from "./components/Error404/Error404";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Notes from "./components/Notes/Notes";
 
 const AppRoutes = () => (
     <App>
-        <Route path="/" component={Home}/>
+        <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/about" component={About} exact/>
+            <Route path="/contact" component={Contact} exact/>
+            <Route path="/notes" component={Notes} exact />
+            <Route path="/notes/:noteId" component={Notes} exact />
+            <Route component={Error404}/>
+        </Switch>
     </App>
 );
 
